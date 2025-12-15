760271d0a57f3a7c29611dfa28ba50122dcc1103
Kojeongsuk <rhwjdtjr2795@naver.com>
2025-12-15 10:21:20 +0000

    test2 commit

--- dist/00_mainServer.js
+++ dist/00_mainServer.js
@@ -14,7 +14,7 @@ const app = (0, express_1.default)();
 const PORT = Number(process.env.PORT) || 5050;
 const allowedOrigins = [
     "http://127.0.0.1:5500",
-    "http://127.0.0.1:5501", // ✅ 실제 Live Server 주소
+    "http://127.0.0.1:5501", // ✅ 실제 Live Server 주소..
     "http://127.0.0.1:5502",
     "http://localhost:5500",
     "http://localhost:5501",
